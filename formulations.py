34e1a4677e6c04fecc5f69c9173099162a6522c6
Jenipher Alum <117021396+Jennyal30@users.noreply.github.com>
2026-04-19 12:03:08 +0300

    Create formulations.py

---
+++ formulations.py
@@ -0,0 +1,13 @@
+# Beverage formulations (real-world inspired)
+
+formulations = {
+    "milk_drink": {
+        "milk": 250,  # ml
+        "protein_source": "dairy"
+    },
+    "oat_pomegranate_drink": {
+        "oat": 200,
+        "pomegranate": 50,
+        "protein_source": "plant"
+    }
+}
