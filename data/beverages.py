0d3ba98cea1872eaca4d5477b6c9de25f86ec2a2
Jenipher Alum <117021396+Jennyal30@users.noreply.github.com>
2026-04-19 12:03:37 +0300

    Create beverages.py

---
+++ data/beverages.py
@@ -0,0 +1,16 @@
+# Nutrient + anti-nutrient composition
+
+beverages = {
+    "milk_drink": {
+        "protein": 3.4,
+        "calcium": 120,
+        "iron": 0.1,
+        "phytate": 0.0
+    },
+    "oat_pomegranate_drink": {
+        "protein": 1.5,
+        "calcium": 80,
+        "iron": 0.6,
+        "phytate": 1.0
+    }
+}
