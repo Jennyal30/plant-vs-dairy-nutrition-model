8854b826813ccca7929cc18ae0b9a376e0894deb
Jenipher Alum <117021396+Jennyal30@users.noreply.github.com>
2026-04-19 14:00:03 +0300

    Update meal_model.py

--- src/src/meal_model.py
+++ src/src/meal_model.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+def build_meal(df, food_list):
+    """
+    Select foods in a meal
+    """
+
+    return df[df["food"].isin(food_list)]
+
+
+def aggregate_meal(meal_df):
+    """
+    Convert food-level → meal-level nutrients
+    """
+
+    return meal_df.sum(numeric_only=True)
